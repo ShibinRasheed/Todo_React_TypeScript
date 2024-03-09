e4b9e212472c96a681b00def89ef553b5d4407dd
ShibinRasheed <shibinrasheed333@gmail.com>
2024-03-09 14:40:03 +0530

    Fixed missed things

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const mainPic = document.querySelector("#mainPic");
 
 const gallery = document.querySelectorAll(".galleryPic");
 
+const btn = document.querySelector("#btn");
+
+const mode = document.querySelector("#mode")
+
+btn.innerText = "Lighten";
+    mode.classList.remove("overlay");
+
+btn.addEventListener("click",()=>{
+  console.log('cli');
+  if(mode.classList[0] == "overlay")
+  {
+    btn.innerText = "Lighten";
+    mode.classList.remove("overlay");
+  }
+  else
+  {
+    btn.innerText = "Darken";
+    mode.classList.add("overlay");
+  }
+ 
+})
+
 for(let pic of gallery){
 pic.addEventListener("click",()=>{
   mainPic.src = pic.src;
